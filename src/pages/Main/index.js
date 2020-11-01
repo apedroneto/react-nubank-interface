3997c73114f3c892eff471be45862bda6feb423d
antonio.abreu <antonio.abreu@virtus.ufcg.edu.br>
2020-11-01 18:43:18 -0300

    Estrutura criada

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,59 +1,50 @@
-import React from 'react';
+import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
-} from 'react-native';
+import { Text, View, SafeAreaView } from 'react-native'
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    paddingHorizontal: 20,
-  },
-  fileName: {
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  instructions: {
-    color: '#DDD',
-    fontSize: 14,
-    marginTop: 20,
-    textAlign: 'center',
-  },
-  logo: {
-    height: Dimensions.get('window').height * 0.11,
-    marginVertical: Dimensions.get('window').height * 0.11,
-    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
-  },
-  welcome: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+import { 
+  Container, 
+  Card, 
+  CardHeader, 
+  Title, 
+  Description, 
+  CardContent,
+  CardFooter, 
+  Annotation ,
+  Content
+} from './styles'
+import Header from '~/components/Header'
+import Tabs from '~/components/Tabs'
+import Menu from '~/components/Menu'
 
-const Main = () => (
-  <ImageBackground
-    source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
-    }}
-    style={styles.container}
-    resizeMode="cover"
-  >
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <Image
-      source={{
-        uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
-      }}
-      style={styles.logo}
-      resizeMode="contain"
-    />
-    <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
-    <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
-    <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
-    <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.js</Text>
-  </ImageBackground>
-);
+export default Main = () => {
 
-export default Main;
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Container>
+        <Header />
+        <Content>
+          <Menu/>
+          <Card>
+            <CardHeader>
+              <Icon name='attach-money' size={28} color='#666'/>
+              <Icon name='visibility-off' size={28} color='#666'/>
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponivel</Title>
+              <Description>R$ 7.611,65</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferencia de R$ 20,00 recebida de Diego Schell Fernandes hoje as 06:00 horas 
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </Content>
+        <Tabs />
+      </Container>
+    </SafeAreaView>
+  )
+
+}
